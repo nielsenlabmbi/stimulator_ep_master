c84e0379a2b255bc7b013b83d589aff9f90f4877
ephysRigMaster <nielsenlabmbi@gmail.com>
2015-10-24 14:45:49 -0400

    Added comment and indentation
    Also added initialize method for GA. No other modules have specific
    initializations.

--- configFiles/moduleListMaster.m
+++ configFiles/moduleListMaster.m
@@ -9,16 +9,17 @@ function outlist = moduleListMaster(listtype)
 %we don't use the config files for the mapper (causes issues with
 %switching from mapper to paramSelect)
 
+% Plist format: Module ID - Module name - Pstate config function - Module specific config requirements method name
 
-Plist{1} = {'PG' 'BW Grating' 'configPstate_PerGrating' };
-Plist{2} = {'RD' 'Random Dot' 'configPstate_RDK' };
-Plist{3} = {'RG' 'RC 1 Grating' 'configPstate_RCGrating' };
-Plist{4} = {'RT' 'RC 2 Gratings' 'configPstate_RC2Gratings' };
-Plist{5} = {'RP' 'RC Grat Plaid' 'configPstate_RCGratPlaid' };
-Plist{6} = {'OF' 'Optic Flow' 'configPstate_OpticFlow' };
-Plist{7} = {'IM' 'Images' 'configPstate_Img' };
-Plist{8} = {'GA' 'GA' 'configPstate_GA' };
-Plist{9} = {'AD' 'Adaptation' 'configPstate_Adapt' };
+Plist{1} = {'PG' 'BW Grating'       'configPstate_PerGrating'   ''};
+Plist{2} = {'RD' 'Random Dot'       'configPstate_RDK'          ''};
+Plist{3} = {'RG' 'RC 1 Grating'     'configPstate_RCGrating'    ''};
+Plist{4} = {'RT' 'RC 2 Gratings'    'configPstate_RC2Gratings'  ''};
+Plist{5} = {'RP' 'RC Grat Plaid'    'configPstate_RCGratPlaid'  ''};
+Plist{6} = {'OF' 'Optic Flow'       'configPstate_OpticFlow'    ''};
+Plist{7} = {'IM' 'Images'           'configPstate_Img'          ''};
+Plist{8} = {'GA' 'GA'               'configPstate_GA'           'initializeModule_GA'};
+Plist{9} = {'AD' 'Adaptation'       'configPstate_Adapt'        ''};
 
 
 Mlist{1} = {'MG' 'BW Grating'};
