4889ab9c854dd84f8e0d6dcb1be597f0abd65e51
ephysRigMaster <nielsenlabmbi@gmail.com>
2015-11-30 18:37:50 -0500

    Manual image module
    Different image modules added for manual mapping

--- GUIs/manualMapper.m
+++ GUIs/manualMapper.m
@@ -133,4 +133,4 @@ sendMinfo
 waitforDisplayResp
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-startStimulus(modId) 
+startManual(modId) 
--- configFiles/moduleListMaster.m
+++ configFiles/moduleListMaster.m
@@ -2,14 +2,13 @@ function outlist = moduleListMaster(listtype)
 
 %list of all modules on the master
 %Plist is the list for the paramSelect window, Mlist for the master window
-%organization: module code (for communication with the slave), description (for parameter GUI), parameter
-%file name
 %the first one is the module that is automatically loaded when starting
 %stimulator
 %we don't use the config files for the mapper (causes issues with
 %switching from mapper to paramSelect)
 
 % Plist format: Module ID - Module name - Pstate config function - Module specific config requirements method name
+% Mlist format: Module ID - Module name
 
 Plist{1} = {'PG' 'BW Grating'       'configPstate_PerGrating'   ''};
 Plist{2} = {'RD' 'Random Dot'       'configPstate_RDK'          ''};
@@ -26,6 +25,7 @@ Plist{10} = {'PC' 'Color Grating'    'configPstate_PerGratingColor'   ''};
 Mlist{1} = {'MG' 'BW Grating'};
 Mlist{2} = {'MM' 'Bar'};
 Mlist{3} = {'MR' 'RDK'};
+Mlist{4} = {'MI' 'Images'};
 
 
 
