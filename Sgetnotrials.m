c9fd0718b0ad357ca350b4b2099a35c53c50b27a
ephysRigMaster <nielsenlabmbi@gmail.com>
2016-01-18 15:27:52 -0500

    bug fixes

--- Sgetnotrials.m
+++ Sgetnotrials.m
@@ -6,6 +6,8 @@ nc = Sgetnoconds;
 
 nt = 0;
 for c = 1:nc
-    nr = length(looperInfo.conds{c}.repeats);
-    nt = nt+nr;
+    nr= Sgetnoreps(c);
+    for r=1:nr
+        nt = nt+ length(looperInfo.conds{c}.repeats{r}.trialno); %necessary to allow for multiple blanks per repeat
+    end
 end
