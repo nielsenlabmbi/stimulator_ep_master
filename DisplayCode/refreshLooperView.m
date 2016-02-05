fe33ad220ca4396b99b14038ad70e8f77501cf95
ephysRigMaster <nielsenlabmbi@gmail.com>
2016-02-04 23:13:55 -0500

    Clear all looper states every time a new looper file is loaded

--- DisplayCode/refreshLooperView.m
+++ DisplayCode/refreshLooperView.m
@@ -1,27 +1,47 @@
 function refreshLooperView
+    global Lstate GUIhandles
 
-global Lstate GUIhandles
+    clearLooperView
+    
+    set(GUIhandles.looper.repeats,'string',Lstate.reps)
+    set(GUIhandles.looper.randomflag,'value',Lstate.rand)
+    set(GUIhandles.looper.blankflag,'value',Lstate.blanks)
 
-set(GUIhandles.looper.repeats,'string',Lstate.reps)
-set(GUIhandles.looper.randomflag,'value',Lstate.rand)
-set(GUIhandles.looper.blankflag,'value',Lstate.blanks)
+    if Lstate.blanks==1
+        set(GUIhandles.looper.blankPeriod,'string',Lstate.blankperiod)
+        set(GUIhandles.looper.blankPeriod,'enable','on')
+    else
+        set(GUIhandles.looper.blankPeriod,'enable','off')
+    end
+    
+    set(GUIhandles.looper.formula,'string',Lstate.formula)
 
-if Lstate.blanks==1
-    set(GUIhandles.looper.blankPeriod,'string',Lstate.blankperiod)
-    set(GUIhandles.looper.blankPeriod,'enable','on')
-else
-    set(GUIhandles.looper.blankPeriod,'enable','off')
+    for i = 1:length(Lstate.param)
+        eval(['symhandle = GUIhandles.looper.symbol' num2str(i) ';'])
+        eval(['valhandle = GUIhandles.looper.valvec' num2str(i) ';'])
+
+        set(symhandle,'string',Lstate.param{i}{1})
+        set(valhandle,'string',Lstate.param{i}{2})
+    end
 end
+
+function clearLooperView
+    global GUIhandles
+
+    set(GUIhandles.looper.repeats,'string','1')
+    set(GUIhandles.looper.randomflag,'value',1)
+    set(GUIhandles.looper.blankflag,'value',1)
     
+    set(GUIhandles.looper.blankPeriod,'string','')
+    set(GUIhandles.looper.blankPeriod,'enable','on')
 
-set(GUIhandles.looper.formula,'string',Lstate.formula)
+    for ii = 1:5
+        eval(['symhandle = GUIhandles.looper.symbol' num2str(ii) ';'])
+        eval(['valhandle = GUIhandles.looper.valvec' num2str(ii) ';'])
+        eval(['currtrihandle = GUIhandles.looper.currtri' num2str(ii) ';'])
 
-for i = 1:length(Lstate.param)
-    
-    eval(['symhandle = GUIhandles.looper.symbol' num2str(i) ';'])
-    eval(['valhandle = GUIhandles.looper.valvec' num2str(i) ';'])
-
-    set(symhandle,'string',Lstate.param{i}{1})
-    set(valhandle,'string',Lstate.param{i}{2})
-
+        set(symhandle,'string','')
+        set(valhandle,'string','')
+        set(currtrihandle,'string','') 
+    end
 end
