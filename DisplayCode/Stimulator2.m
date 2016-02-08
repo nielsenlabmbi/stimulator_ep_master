1717aa5ea6da8926e4a7c4d71c3b3675d7084890
ephysRigMaster <nielsenlabmbi@gmail.com>
2016-02-08 12:08:30 -0500

    Adding z drive in analyzer file default location
    Also repositioning the windows to be tighter because we added GUI
    elements.

--- DisplayCode/Stimulator2.m
+++ DisplayCode/Stimulator2.m
@@ -20,10 +20,10 @@ hm = MainWindow;
 movegui(hm,[10,500]);
 
 hl = Looper ;
-movegui(hl,[380,445]);
+movegui(hl,[365,445]);
 
 hp = paramSelect;
-movegui(hp,[10,170]);
+movegui(hp,[10,110]);
 
 hmm = manualMapper;
-movegui(hmm,[380,240]);
+movegui(hmm,[365,275]);
--- DisplayCode/configureMstate.m
+++ DisplayCode/configureMstate.m
@@ -9,14 +9,18 @@ Mstate.expt = '000';
 Mstate.hemi = 'left';
 Mstate.screenDist = 25;
 
-Mstate.monitor = 'VSN';  %This should match the default value in Display
+% initialize setup specific Mstate values
+if strcmp(getSetup,'EP')
+    Mstate.monitor = 'VSN';  %This should match the default value in Display
+    Mstate.analyzerRoot = 'C:\VStimFiles\AnalyzerFiles; Z:\Ephys\AnalyzerFiles';
+else
+    Mstate.monitor = 'LCD';  %This should match the default value in Display
+    Mstate.analyzerRoot = 'C:\VStimFiles\AnalyzerFiles; Z:\2P\Analyzer';
+end
 
 updateMonitorValues
 
 Mstate.syncSize = 4;  %Size of the screen sync in cm
 
 Mstate.running = 0;
-
-%Mstate.analyzerRoot = ['C:\VStimFiles\AnalyzerFiles' ' ; ' '\\ACQUISITION\neurostuff\AnalyzerFiles'];
-Mstate.analyzerRoot = 'C:\VStimFiles\AnalyzerFiles';
 Mstate.dataRoot='c:\data';
