68ff7c8fe140b607680e7ce21bd3170fda81324b
bhrig1 <nielsenlabmbi@gmail.com>
2018-03-29 12:07:30 -0400

    Adding support for formulae with [] = [];

--- DisplayCode/buildStimulus.m
+++ DisplayCode/buildStimulus.m
@@ -125,3 +125,38 @@ if ~isempty(idx)  %its possible that looper variable is not a grating parameter
     end
 end
 
+
+% if psymbol contains multiple variables, like: [ori,x_pos,y_pos]
+% this only works if the values corresponding to these variables are single
+% numbers. not strings or matrices.
+if (psymbol(1) == '[' && psymbol(end) == ']')
+    psymbol = psymbol(2:end-1);
+    commas = strfind(psymbol,',');
+    vars = textscan(psymbol,repmat('%s',1,length(commas)+1),'delimiter',',');
+    if length(vars) == length(pval)
+        for ii=1:length(vars)
+            %Find parameter in Pstruct
+            idx = [];
+            for j = 1:length(Pstate.param)
+                if strcmp(vars{ii}{1},Pstate.param{j}{1})
+                    idx = j;
+                    break;
+                end
+            end
+
+            %change value based on looper
+            if ~isempty(idx)  %its possible that looper variable is not a grating parameter
+                prec = Pstate.param{idx}{2};  %Get precision
+                switch prec
+                    case 'float'
+                        msg = sprintf('%s;%s=%.4f',msg,vars{ii}{1},pval(ii));
+                    case 'int'
+                        msg = sprintf('%s;%s=%d',msg,vars{ii}{1},round(double(pval(ii))));
+                end
+            end
+        end
+    else
+        disp('Could not parse variables in the formula. Please debug.');
+    end
+end
+
