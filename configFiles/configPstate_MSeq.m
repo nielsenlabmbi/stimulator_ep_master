d24a07669c1b4fb7ee023ded3554ad6d07a1dce0
bhrig1 <nielsenlabmbi@gmail.com>
2019-09-24 14:08:07 -0400

    M seq

--- configFiles/configPstate_MSeq.m
+++ configFiles/configPstate_MSeq.m
@@ -14,10 +14,10 @@ Pstate.param{4} = {'x_pos'       'int'      600       0                'pixels'}
 Pstate.param{5} = {'y_pos'       'int'      400       0                'pixels'};
 Pstate.param{6} = {'size'      'float'      3       1                'deg'};
 
-Pstate.param{7} = {'seqfile'      'string'     '~/'       1                ''};
+Pstate.param{7} = {'seqfile'      'string'     '~/stimulator_slave/makeTextures/utils/MSequence.mat'       1                ''};
 
-Pstate.param{8} = {'h_per'      'int'   2      0                'frames'};
-Pstate.param{9} = {'startOffset'    'int'   0       0                'sequence start'};
+Pstate.param{8} = {'h_per'      'int'   3      0                'frames'};
+Pstate.param{9} = {'startOffset'    'int'   0       0                'frames'};
 
 Pstate.param{10} = {'background'      'float'   0.5       0                ''};
 
